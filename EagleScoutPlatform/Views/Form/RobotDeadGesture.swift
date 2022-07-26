47bf8dd3d9cf5eb283dc53e64e90e669efc92ef2
Vir Shah <virn.shah@gmail.com>
2022-07-26 23:58:43 +0200

    separates climb time and dead time into two separate timers

--- EagleScoutPlatform/Views/Form/RobotDeadGesture.swift
+++ EagleScoutPlatform/Views/Form/RobotDeadGesture.swift
@@ -17,6 +17,12 @@ struct RobotDeadGesture: View {
     }
     @State var a = ScoutingReport()
     
+    // custom view variables
+    @State var deadTimerOn: Bool = false
+    @State var climberTimerOn: Bool = false
+    
+    @State var climbTime: Double = 0
+    
     // TODO: Add a timer to track when a robot is in the climbing phase
     
     var body: some View {
@@ -24,26 +30,129 @@ struct RobotDeadGesture: View {
             ScoutReportView(report: $a)
                 .gesture(tap)
                 .onChange(of: self.tapped) { newValue in
-                    a.dead = true
-                    a.timeDead = self.timeDead
+                    if self.timeDead > 0 {
+                        a.dead = true
+                        a.timeDead = self.timeDead
+                    }
+                    if self.climbTime > 0 {
+                        a.attemptedClimb = true
+                        a.climbTime = self.climbTime
+                    }
                 }
             if self.tapped {
-                Rectangle()
-                    .fill(Color.red)
-                    .ignoresSafeArea(.container, edges: .top)
-                    .gesture(tap)
-                    .transition(.scale.animation(.easeIn))
-                VStack {
-                    Text("Time Dead:")
-                        .font(.title)
-                    Text("\(timeDead, specifier: "%.2f")")
-                        .monospacedDigit()
-                        .font(.largeTitle)
-                        .padding()
-                        .onReceive(timer) { _ in
-                            timeDead += 0.01
+                if deadTimerOn {
+                    Rectangle()
+                        .fill(Color.red)
+                        .ignoresSafeArea()
+//                        .transition(.slide.animation(.linear))
+                    VStack {
+                        Text("Time Dead:")
+                            .font(.title)
+                        Text("\(timeDead, specifier: "%.2f")")
+                            .monospacedDigit()
+                            .font(.largeTitle)
+                            .padding()
+                            .onReceive(timer) { _ in
+                                timeDead += 0.01
+                            }
+                    }
+                    Button {
+                        deadTimerOn = false
+                        self.tapped = false
+                    } label: {
+                        VStack {
+                            Spacer()
+                            Image(systemName: "x.circle")
+                                .font(.title2)
+                                .padding()
                         }
+                    }.buttonStyle(.plain)
+
+                } else if climberTimerOn {
+                    Rectangle()
+                        .fill(Color.blue)
+                        .ignoresSafeArea()
+                    
+                    VStack {
+                        Text("Time to Climb:")
+                            .font(.title)
+                        Text("\(climbTime, specifier: "%.2f")")
+                            .monospacedDigit()
+                            .font(.largeTitle)
+                            .padding()
+                            .onReceive(timer) { _ in
+                                climbTime += 0.01
+                            }
+                    }
+                    Button {
+                        climberTimerOn = false
+                        self.tapped = false
+                    } label: {
+                        VStack {
+                            Spacer()
+                            Image(systemName: "x.circle")
+                                .font(.title2)
+                                .padding()
+                        }
+                    }.buttonStyle(.plain)
+                    
+                } else {
+                    Rectangle()
+                        .fill(Color.custom1)
+                        .ignoresSafeArea()
+                        .gesture(tap)
+                        .transition(.scale.animation(.easeIn))
+                    HStack {
+                        Spacer()
+                        Button {
+                            deadTimerOn = true
+                        } label: {
+                            VStack {
+                                ZStack {
+                                    Circle()
+                                        .opacity(0.5)
+                                        .frame(width: 100, height: 100, alignment: .center)
+                                    Image(systemName: "exclamationmark.triangle.fill")
+//                                        .foregroundColor(.yellow)
+                                        .font(.largeTitle)
+                                }
+                                Text("Time Dead")
+                                    .font(.caption)
+                            }
+                        }.buttonStyle(.plain)
+
+                        Spacer()
+                        Button {
+                            climberTimerOn = true
+                        } label: {
+                            VStack {
+                                ZStack {
+                                    Circle()
+                                        .opacity(0.5)
+                                        .frame(width: 100, height: 100, alignment: .center)
+                                    Image(systemName: "chart.bar.fill")
+//                                        .foregroundColor(Color.custom1)
+                                        .font(.largeTitle)
+                                }
+                                Text("Climb Time")
+                                    .font(.caption)
+                            }
+                        }.buttonStyle(.plain)
+
+                        Spacer()
                 }.transition(.scale.animation(.easeIn))
+                // button to exit view
+                Button {
+                    tapped = false
+                } label: {
+                    VStack {
+                        Spacer()
+                        Image(systemName: "x.circle")
+                            .font(.title2)
+                            .padding()
+                    }
+                }.buttonStyle(.plain)
+                }
             }
         }
     }
