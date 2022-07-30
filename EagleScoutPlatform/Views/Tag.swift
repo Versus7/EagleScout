9664f4f53105bbd1bb06ccbe7d89edce1ec51c53
Vir Shah <virn.shah@gmail.com>
2022-07-30 18:41:46 +0100

    Cleans up tag code

--- EagleScoutPlatform/Views/Tag.swift
+++ EagleScoutPlatform/Views/Tag.swift
@@ -12,12 +12,6 @@ struct Tag: View {
     @State var isPressed: Bool = false
     @Binding var listOfTags: Set<String>
     var body: some View {
-//        Capsule()
-//            .fill(Color.red)
-//            .opacity(0.5)
-//            .background(Text("HELLO"))
-//            .frame(width: 200, height: 100, alignment: .center)
-//            .cornerRadius(10)
         Button {
             isPressed.toggle()
             if isPressed {
@@ -26,11 +20,8 @@ struct Tag: View {
                 listOfTags.remove(text)
             }
         } label: {
-//            Label(text, systemImage: isPressed ? "checkmark" : "")
             Text(self.text)
-//                .padding()
-//                .fill(Color.red)
-//            Capsule()
+                .font(.system(.body, design: .rounded))
         }.buttonStyle(.borderedProminent)
             .cornerRadius(25)
             .tint(self.isPressed ? .custom1 : .gray)
