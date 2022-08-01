0065e49d85ef3ad5b12db5ef430f9b7f645a124c
Vir Shah <virn.shah@gmail.com>
2022-08-01 11:41:02 -0700

    Redone using the custom Incrementer class.

--- EagleScoutPlatform/Views/Form/LargeIncrementer.swift
+++ EagleScoutPlatform/Views/Form/LargeIncrementer.swift
@@ -11,84 +11,51 @@ struct LargeIncrementer: View {
     @Binding var high: Int
     @Binding var low: Int
     var body: some View {
-        ZStack {
-            VStack(alignment: .center, spacing: 2) {
-                GroupBox {
-                    VStack {
-                        HStack {
-                            Spacer()
-                            Text("High")
-                                .font(.headline)
-                            Spacer()
-                            Text(String(high)).monospacedDigit()
-                                .font(.largeTitle)
-                            Spacer()
-                        }.padding(.horizontal)
-                        HStack {
-                            Spacer()
-                            Text("Low")
-                                .font(.headline)
-                            Spacer()
-                            Text(String(low)).monospacedDigit()
-                                .font(.largeTitle)
-                            Spacer()
-                        }
-                        .padding(.horizontal)
-                    }
-                } label: {
-                    Text("Score")
-                }
-                .padding()
-                Rectangle()
-                    .opacity(0)
-                HStack(alignment: .center, spacing: 2) {
-                    Button {
-                        if high > 0 {
-                            high -= 1
+        GeometryReader { reader in
+            ZStack {
+                VStack(alignment: .center, spacing: 2) {
+                    GroupBox {
+                        VStack {
+                            HStack {
+                                Spacer()
+                                Text("High")
+                                    .font(.headline)
+                                Spacer()
+                                Text(String(high)).monospacedDigit()
+                                    .font(.largeTitle)
+                                Spacer()
+                            }.padding(.horizontal)
+                            HStack {
+                                Spacer()
+                                Text("Low")
+                                    .font(.headline)
+                                Spacer()
+                                Text(String(low)).monospacedDigit()
+                                    .font(.largeTitle)
+                                Spacer()
+                            }
+                            .padding(.horizontal)
                         }
                     } label: {
-                        IncrementerButtonLabel(fillColor: .red, imageName: "minus")
+                        Text("Score")
                     }
-                    Button {
-                        high += 1
-                    } label: {
-                        IncrementerButtonLabel(fillColor: .green, imageName: "plus")
-                    }
-                }
-                HStack(alignment: .center, spacing: 2) {
-                    Button {
-                        if low > 0 {
-                            low -= 1
-                        }
-                    } label: {
-                        IncrementerButtonLabel(fillColor: .red, imageName: "minus")
-                    }
-                    Button {
-                        low += 1
-                    } label: {
-                        IncrementerButtonLabel(fillColor: .green, imageName: "plus")
-                    }
+                    .padding()
+                    Spacer()
+                    Incrementer(value: $high, iconColor: .primary, width: reader.size.width*0.85, height: reader.size.width*0.3, imageSize: .title)
+                    Incrementer(value: $low, iconColor: .primary, width: reader.size.width*0.85, height: reader.size.width*0.3, imageSize: .title)
+                        .padding(.bottom)
                 }
             }
         }
     }
 }
 
-struct IncrementerButtonLabel: View {
-    let fillColor: Color
-    let imageName: String
-    var body: some View {
-        Rectangle()
-            .opacity(0)
-            .border(fillColor, width: 4)
-            .overlay(Image(systemName: imageName).imageScale(.large))
-            .foregroundColor(fillColor)
-    }
-}
 struct LargeIncrementer_Previews: PreviewProvider {
-    @State static var a = 0
+    @State static var a = 7
     @State static var b = 0
     static var previews: some View {
+        LargeIncrementer(high: $a, low: $b)
+            .preferredColorScheme(.light)
         LargeIncrementer(high: $a, low: $b)
             .preferredColorScheme(.dark)
     }
