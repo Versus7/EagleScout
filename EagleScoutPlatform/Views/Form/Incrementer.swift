5e0adf01f52efca7a3bad25494abf2d27556e6cc
Vir Shah <virn.shah@gmail.com>
2022-08-01 11:38:13 -0700

    Allows user to control width and height of the control, as well as customize the size of the images. This adds light/dark mode support, and changes the opacity of the minus button when the value is 0 to give the disabled look.

--- EagleScoutPlatform/Views/Form/Incrementer.swift
+++ EagleScoutPlatform/Views/Form/Incrementer.swift
@@ -11,13 +11,14 @@ struct Incrementer: View {
     @Binding var a: Int
     let spacerColor: Color
     let iconColor: Color
-    private let width: CGFloat = 100
-    private let height: CGFloat = 35
+    private var width: CGFloat = 100
+    private var height: CGFloat = 35
+    private var imageSize: Font = .body
     
     init(value: Binding<Int>) {
         self._a = value
         self.spacerColor = .gray
-        self.iconColor = .black
+        self.iconColor = .primary
     }
     
     init(value: Binding<Int>, iconColor: Color) {
@@ -26,10 +27,11 @@ struct Incrementer: View {
         self.spacerColor = .gray
     }
     
-    init(value: Binding<Int>, background: Color) {
-        self._a = value
-        self.spacerColor = background
-        self.iconColor = .gray
+    init(value: Binding<Int>, iconColor: Color, width: CGFloat, height: CGFloat, imageSize: Font) {
+        self.init(value: value, iconColor: iconColor)
+        self.width = width
+        self.height = height
+        self.imageSize = imageSize
     }
     
     init(value: Binding<Int>, iconColor: Color, background: Color) {
@@ -55,15 +57,18 @@ struct Incrementer: View {
                                 }
                             } label: {
                                 Image(systemName: "minus")
+                                    .font(imageSize)
                                     .foregroundColor(iconColor)
                                     .frame(width: width/2, height: height, alignment: .center)
                             }
+                            .opacity(a == 0 ? 0.3 : 1)
                                 .disabled(a == 0)
                             Divider()
                             Button {
                                 a += 1
                             } label: {
                                 Image(systemName: "plus")
+                                    .font(imageSize)
                                     .foregroundColor(iconColor)
                                     .frame(width: width/2, height: height, alignment: .center)
                             }
